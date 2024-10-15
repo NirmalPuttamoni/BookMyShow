6d81b20419c74328f2ac85d2645494bc2bc213a8
Nirmal <puttamoninirmal01@gmail.com>
2024-10-15 10:46:07 +0530

    call frontend

--- server/server.js
+++ server/server.js
@@ -1,20 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
+const path = require("path");
+const cors = require("cors");
+
 const userRouter = require("./routes/userRouter");
 const showRouter = require("./routes/showRouter");
 const movieRouter = require("./routes/movieRouter");
 const theatreRouter = require("./routes/theatreRouter");
 const bookingRouter = require("./routes/bookingRouter");
+
 const port = process.env.PORT;
 const app = express();
-app.use(express.json())
+
+app.use(cors({
+    origin: "*",
+    methods: ["*"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+app.use(express.json());
 // require("dotenv").config(); // load env variables into process.env
 
 /**
  * to read from env file, we use a package called dotenv
  * what it does is, it reads the .env file and populates the process.env object
  */
+
+const clientBuildPath = path.join(__dirname, "../client/build");
+console.log(clientBuildPath);
+app.use(express.static(clientBuildPath));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(clientBuildPath, "index.html"));
+// });
 
 connectDB();
 
@@ -30,11 +49,11 @@ connectDB();
  */
 
 app.use("/api/users", userRouter);
-app.use("/api/movies", movieRouter)
-app.use("/api/theatres", theatreRouter)
-app.use("/api/shows", showRouter)
-app.use("/api/bookings", bookingRouter)
+app.use("/api/movies", movieRouter);
+app.use("/api/theatres", theatreRouter);
+app.use("/api/shows", showRouter);
+app.use("/api/bookings", bookingRouter);
 
 app.listen(port, () => {
-    console.log("Server started at port :", port);
-})
+  console.log("Server started at port :", port);
+});
